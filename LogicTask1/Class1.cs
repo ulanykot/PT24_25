ccbb19c177392991dd7b17f3138fc1b8d637f4ca
247044 Wiktoria Świder <247044@edu.p.lodz.pl>
2024-04-15 08:10:03 +0200

    logic HotelManager class implemented

--- LogicTask1/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LogicTask1
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ LogicTask1/HotelManager.cs
@@ -0,0 +1,54 @@
+﻿using DataTask1;
+
+namespace LogicTask1
+{
+    public class HotelManager
+    {
+
+        private DataRepository _dataRepository;
+
+        public HotelManager(DataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        public void AddUser(User user)
+        {
+            _dataRepository.AddUser(user);
+        }
+
+        public void AddRoomToCatalog(int id, Catalog room)
+        {
+            var state = new State(room, 0); // Initialize room state with a price of 0
+            _dataRepository.AddState(state);
+        }
+
+        public void MakeBooking(Guest guest, State room, DateTime checkInDate, DateTime checkOutDate)
+        {
+            var bookingEvent = new BookingEvent(guest, room)
+            {
+                CheckInDate = checkInDate, 
+                CheckOutDate = checkOutDate
+            };
+            _dataRepository.AddEvent(bookingEvent);
+        }
+
+        public void CheckInGuest(Guest guest, State room, DateTime checkInDate)
+        {
+            var checkInEvent = new CheckInEvent(guest, room)
+            {
+                CheckInDate = checkInDate
+            };
+            _dataRepository.AddEvent(checkInEvent);
+        }
+
+        public void CheckOutGuest(User guest, State room, DateTime checkOutDate)
+        {
+            var checkOutEvent = new CheckOutEvent(guest, room)
+            {
+                CheckOutDate = checkOutDate
+            };
+            _dataRepository.AddEvent(checkOutEvent);
+        }
+    }
+}
