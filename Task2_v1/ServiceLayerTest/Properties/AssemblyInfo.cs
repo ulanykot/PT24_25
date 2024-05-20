d11254f0ed44e9272d33f3cde64d538c399df12c
vevivici <swider.wik@gmail.com>
2024-05-21 00:16:21 +0200

    ServiceLayer
    refactoring on ServiceLayer and addition of ServiceLayerTest project

---
+++ Task2_v1/ServiceLayerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServiceLayerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceLayerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73727331-6ae9-40ba-b7d5-03e88550d04b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task2_v1/ServiceLayerTest/ServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using ServiceLayer;
+
+[TestClass]
+public class CatalogServiceTests
+{
+    
+
+}
