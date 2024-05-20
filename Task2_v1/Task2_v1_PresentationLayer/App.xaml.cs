323bc364e7316dac573be0862457319ac2636f9d
vevivici <swider.wik@gmail.com>
2024-05-20 17:18:37 +0200

    addition of presentation layer

---
+++ Task2_v1/Task2_v1_PresentationLayer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Task2_v1_PresentationLayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
